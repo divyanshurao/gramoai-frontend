748ab57dacede277660a3586341596478757b46a
divyanshurao <divyanshu.co19@nsut.ac.in>
2025-03-08 01:48:41 +0530

    fix ui

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-
+import logo from "./../../assets/logo.png";
 
 
 const Navbar: React.FC = () => {
   return (
     <div className="flex justify-end items-start p-2 mb-1 bg-[#5753F2] border w-full h-28">
-        <img src="src\assets\logo.png" alt="Image" className="h-24 w-24 absolute left-10 top-2" />
+        <img src={logo} alt="Image" className="h-24 w-24 absolute left-10 top-2" />
     </div>
   );
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,7 +9,7 @@ const Home = () => {
       <Navbar />
       <div className="container mx-auto px-4 pt-24 md:pt-32 pb-20">
         <div className="flex flex-col items-center justify-center text-center max-w-4xl mx-auto">
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-semibold tracking-tight text-gray-800 mt-28 mb-8">
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-semibold tracking-tight text-gray-800 mt-16 mb-8">
             Get a content calendar tailored for your LinkedIn audience.
           </h1>
           <button
