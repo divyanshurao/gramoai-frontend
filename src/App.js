14fe30777fcff6e2be7a8d3e2fdee9e94073a446
divyanshurao <divyanshu.co19@nsut.ac.in>
2025-03-07 23:57:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+      <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700">
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
