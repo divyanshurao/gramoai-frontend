c5b78dd8700c65fd156bd137ce1f2cf851629b69
neeraj-22 <gandhineeraj48@gmail.com>
2025-03-14 21:32:33 +0530

    authToken update

--- src/api/config.ts
+++ src/api/config.ts
@@ -22,5 +22,5 @@ export const CONFIG = {
       HUMANIZER:"/api/humanizer"
     }
   },
-  AUTH_TOKEN: getCookie('auth_token') || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY3RpdmVUZWFtIjp7ImlkIjoidGVhLTEyMyIsIm5hbWUiOiJEZWZhdWx0IFRlYW0iLCJwcml2aWxlZ2UiOnsiYWRtaW4iOnRydWUsInVzZXIiOmZhbHNlfX0sImV4cCI6MTc1MDMwNzU4MiwiaWF0IjoxNzQwMzAyOTgyLCJpc3MiOiJwb3N0LWFnZW50IiwidGVhbXMiOlt7ImlkIjoidGVhLTEyMyIsIm5hbWUiOiJEZWZhdWx0IFRlYW0iLCJwcml2aWxlZ2UiOnsiYWRtaW4iOnRydWUsInVzZXIiOmZhbHNlfX1dLCJ1c2VyIjp7ImVtYWlsIjoiZ2FuZGhpbmVlcmFqMDhAZ21haWwuY29tIiwiaWQiOiJ1cy0xNzBmYzRhOC1kNzFmLTQwODMtYmQ2Ni04YTE1ZTI3MGUwZTgiLCJuYW1lIjoiTmVlcmFqIEdhbmRoaSJ9fQ.admDHEe8PMNAiw-rIp7HE2xswS2sgK8alkDzXqc_uKE"
+  AUTH_TOKEN: getCookie('auth_token')
 };
